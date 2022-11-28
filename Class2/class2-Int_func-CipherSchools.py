4efc41602df78826db4050e2efc45757ffe37844
COdestriker05 <114793925+COdestriker05@users.noreply.github.com>
2022-11-28 13:01:42 +0530

    Add files via upload

---
+++ Class2/class2-Int_func-CipherSchools.py
@@ -0,0 +1,12 @@
+# Int function
+
+number_one = input("Enter first number ")
+number_two = input("Enter second number ")
+total = number_one + number_two  # It concate as string not add as integer
+print("Total is " + total)
+# total = '4' + '4' = '44'  
+
+number_one = int(input("Enter first number "))
+number_two = int(input("Enter second number "))
+total = number_one + number_two
+print("Total is " + str(total))
