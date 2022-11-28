567fcf7ae9aea5201444e7b39e7c35157198653f
COdestriker05 <114793925+COdestriker05@users.noreply.github.com>
2022-11-28 19:07:11 +0530

    Add files via upload

---
+++ Class2/class2-String_method2-CipherSchools.py
@@ -0,0 +1,22 @@
+# String methods part two-
+
+#                          replace() method-
+string = "she is beautiful and she is good dancer"
+print(string.replace("is","was",1)) # we can also decide how many time we want to replace anything in the string.
+
+#                          find() method-
+print(string.find("is",7)) # we can also decide from where we want to start to find our required string.
+
+# if we don't  know from where should we start then we can do;
+is_pos1 = string.find("is")
+is_pos2 = string.find("is", is_pos1 + 1)
+print(is_pos2)
+
+#                          center() method-
+name = "Shivam"
+
+# output- **Shivam**
+print(name.center(14,"*"))
+
+name = input("Enter your name : ")
+print(name.center(len(name)+8,"*"))
