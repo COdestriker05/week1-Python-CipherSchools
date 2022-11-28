e1d833a19a54071f85935c75959096a55a19b93e
COdestriker05 <114793925+COdestriker05@users.noreply.github.com>
2022-11-28 13:04:03 +0530

    Update class2-Int_func-CipherSchools.py

--- Class2/class2-Int_func-CipherSchools.py
+++ Class2/class2-Int_func-CipherSchools.py
@@ -10,3 +10,12 @@ number_one = int(input("Enter first number "))
 number_two = int(input("Enter second number "))
 total = number_one + number_two
 print("Total is " + str(total))
+
+# str function- str()
+# 4 ---> "4"
+
+# int function- int()
+# "4" ---> 4
+
+# float function- float()
+# "4" ---> 4.0
